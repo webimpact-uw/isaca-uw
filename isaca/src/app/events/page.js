d1aaed05c33ddfba13cb03ecb1229725b5ec0ae8
bbgmqus <150094417+bbgmqus@users.noreply.github.com>
2025-04-16 04:12:24 -0700

    Add events folder with events page

--- isaca/src/app/events/page.js
+++ isaca/src/app/events/page.js
@@ -1,3 +1,13 @@
 export default function Events() {
-    return <p>Test events page</p>;
+    return  <div className="flex justify-center items-center min-h-screen">
+    <iframe
+      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&src=bXBpbGFwaWxAdXcuZWR1&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
+      style={{ border: '1px solid #777' }}
+      width="800"
+      height="600"
+      frameBorder="0"
+      scrolling="no"
+      title="Google Calendar"
+    ></iframe>
+  </div>
 }
