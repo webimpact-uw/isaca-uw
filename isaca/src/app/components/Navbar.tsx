f4f20e8e91e093ef5a7f74312d45277be1edf78e
juliettepark <69448181+juliettepark@users.noreply.github.com>
2025-04-15 18:14:06 -0700

    fixed logo size

--- isaca/src/app/components/Navbar.tsx
+++ isaca/src/app/components/Navbar.tsx
@@ -6,21 +6,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
       <nav className='flex justify-between'>
-        <img src='logo.png' className='ml-7'></img>
-        {/* <Image
-          src="logo.png"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-        /> */}
-        {/* <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
+        <img src='logo.png' className='ml-7 w-40'></img>
 
         <ul className="flex justify-end items-center">
             <li className="mr-7"><Link href='./'>Home</Link></li>
