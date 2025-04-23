6a6f908fbee7351acb1f94f4dc5e2b3007888776
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2025-04-22 18:36:44 -0700

    global.css font updates - added dm sans, dm mono - added some color vars

--- isaca/src/app/events/page.js
+++ isaca/src/app/events/page.js
@@ -1,13 +1,16 @@
 export default function Events() {
-    return  <div className="flex justify-center items-center min-h-screen">
-    <iframe
-      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&src=bXBpbGFwaWxAdXcuZWR1&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
-      style={{ border: '1px solid #777' }}
-      width="800"
-      height="600"
-      frameBorder="0"
-      scrolling="no"
-      title="Google Calendar"
-    ></iframe>
-  </div>
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1>Events</h1>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&src=bXBpbGFwaWxAdXcuZWR1&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
+        style={{ border: '1px solid #777' }}
+        width="800"
+        height="600"
+        frameBorder="0"
+        scrolling="no"
+        title="Google Calendar"
+      ></iframe>
+    </div>
+  )
 }
