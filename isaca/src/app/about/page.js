3e8ff7eacf96029914a472d6e3db18b4464ee5de
Rocky Zhou <rzhou16@uw.edu>
2025-04-26 12:47:10 -0700

    Added about page components

--- isaca/src/app/about/page.js
+++ isaca/src/app/about/page.js
@@ -1,9 +1,10 @@
       export default function About() {
         return (
                 <div style={{padding: "60px 20px"}}>
-
+                <center>
                 <h1>About Us</h1>
                 <p>Test about page</p>
+                </center>
                 </div>
             
             
