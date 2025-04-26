a0fb8a161ef774541cda24f9c920bb85e7200308
Rocky Zhou <rzhou16@uw.edu>
2025-04-26 00:26:03 -0700

    Add About page layout with basic heading

--- isaca/src/app/about/page.js
+++ isaca/src/app/about/page.js
@@ -1,3 +1,12 @@
-export default function About() {
-    return <p>Test about page</p>;
-}
+      export default function About() {
+        return (
+                <div style={{padding: "60px 20px"}}>
+
+                <h1>About Us</h1>
+                <p>Test about page</p>
+                </div>
+            
+            
+          
+        );
+      }
