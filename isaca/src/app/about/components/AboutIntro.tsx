f1d02ff06b2d64677855e74875c7bdf327adbb8f
Rocky Zhou <rzhou16@uw.edu>
2025-04-28 15:06:03 -0700

    Added intro text

--- isaca/src/app/about/components/AboutIntro.tsx
+++ isaca/src/app/about/components/AboutIntro.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 const AboutIntro : React.FC = () => { 
     return(
-        <section style = {{padding: "60px 20px" }}>
+        <section style = {{padding: "60px 20px",textAlign: "center" }}>
             
             
             <h1 style={{color:'#0C2A64'}}>About Us</h1>
-            <p>Founded in 2016, ISACA UW is dedicated...</p>
-
+            <p>Founded in 2016, ISACA UW is dedicatedFounded in 2016, 
+                ISACA UW is dedicated to promoting excellence in information
+                 security by providing quality programming for current University
+                  of Washington students to interact with industry leaders and gain 
+                  hands-on learning experiences. We host quarterly speaker and workshop
+                   events. All majors, programs, and backgrounds are welcomed!</p>
+            <div>
+                <button>Puget Sound Chapter</button>
+                <button>Global Chapter</button>
+            </div>
 
         </section>
 
@@ -15,3 +23,5 @@ const AboutIntro : React.FC = () => {
     );
 
 };
+
+export default AboutIntro;
