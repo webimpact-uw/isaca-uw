b4c42f3146797074f05ae6781179e968dd2e8334
ssivak2 <ssivak2@uw.edu>
2025-04-29 18:28:58 -0700

    Fixed the duplicate Navbar files

--- isaca/src/app/components/Navbar.tsx
+++ isaca/src/app/components/Navbar.tsx
@@ -1,32 +1,15 @@
-import logo from '../assets/logo.png';
-import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Navbar() {
-    return (
-      <nav className='flex justify-between'>
-        <img src='logos/logo.png' className='ml-7 w-40'></img>
-        {/* <Image
-          src="logo.png"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-        /> */}
-        {/* <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
+  return (
+    <nav className='flex justify-between'>
+      <img src='logos/logo.png' alt="ISACA logo" className='ml-7 w-40' />
 
-        <ul className="flex justify-end items-center">
-            <li className="mr-7"><Link href='./'>Home</Link></li>
-            <li className="mr-7"><Link href='./about'>About</Link></li>
-            <li className="mr-7"><Link href='./events'>Events</Link></li>
-        </ul>
-      </nav>
-    );
-  }
+      <ul className="flex justify-end items-center">
+          <li className="mr-7"><Link href='/'>Home</Link></li>
+          <li className="mr-7"><Link href='/about'>About</Link></li>
+          <li className="mr-7"><Link href='/events'>Events</Link></li>
+      </ul>
+    </nav>
+  );
+}
