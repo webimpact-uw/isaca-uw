0855e6899a5285d808ba2d5af1e03f28263f63ed
bbgmqus <150094417+bbgmqus@users.noreply.github.com>
2025-05-12 17:13:27 -0700

    replaced with new calendar

---
+++ isaca/src/app/events/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Events() {
+  return (
+    <div className="flex flex-col items-center pt-10 min-h-screen">
+      <h1 className="text-4xl font-bold-DM mono text-[#0C2A64] mb-2">Events</h1>
+      <p className="text-gray-500 text-md mt-2 pb-10 font-DM Sans">
+        Check out upcoming events. We do about 3-4 events a quarter! ...
+      </p>
+      <hr className="w-5/6 border-t border-gray-700 my-4 pb-10" />
+        <iframe
+        id="open-web-calendar"
+        style={{
+          background: "url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat",
+          width: "100%",
+          height: "500px",
+          border: "none"
+        }}
+        src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?controls=next&controls=previous&controls=date&skin=flat&tabs=&url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F9caa3bc6075136a8221ba12bc282faa66eefbd514d5634c16ccee8af61295c2d%2540group.calendar.google.com%2Fpublic%2Fbasic.ics"
+        sandbox="allow-scripts allow-same-origin allow-top-navigation allow-downloads"
+        scrolling="no"
+      />
+    </div>
+  );
+}
