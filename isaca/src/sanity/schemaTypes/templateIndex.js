f1c4d865d209367d683d42a692f010f65473d0eb
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2025-05-20 14:42:43 -0700

    starting to create Sanity schemas

---
+++ isaca/src/sanity/schemaTypes/templateIndex.js
@@ -0,0 +1,8 @@
+import {blockContentType} from './templateBlockContentType'
+import {categoryType} from './templateCategoryType'
+import {postType} from './postType'
+import {authorType} from './templateAuthorType'
+
+export const schema = {
+  types: [blockContentType, categoryType, postType, authorType],
+}
