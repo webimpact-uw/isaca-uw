066741a04864e258cc4750664724f9fc4e42796e
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2025-05-27 15:32:16 -0700

    Part 1 of mobile responsiveness changes

--- isaca/src/app/components/Navbar.tsx
+++ isaca/src/app/components/Navbar.tsx
@@ -8,16 +8,40 @@ export default function Navbar() {
   const isActive = (path: string) => pathname === path;
 
   return (
-    <nav className='flex justify-between py-4 border-solid border-b-1 border-lighter-gray'>
-      <Link href='/'>
-        <img src='logos/isaca-horizontal-main.png' alt="ISACA logo" className='ml-7 w-40' />
-      </Link>
+    <nav className='flex justify-between items-center py-4 border-solid border-b-1 border-lighter-gray'>
+      <section>
+        <Link href='/'>
+          <img src='logos/isaca-horizontal-main.png' alt="ISACA logo" className='ml-7 w-40' />
+        </Link>
+      </section>
 
-      <ul className="flex justify-end items-center text-navy-blue text-l mr-7 font-medium font-mono">
-          <li className={`mr-5 underline-offset-3 ${isActive("/") ? "underline" : "hover:underline"}`}><Link href='/'>Home</Link></li>
-          <li className={`mr-5 underline-offset-3 ${isActive("/about") ? "underline" : "hover:underline"}`}><Link href='/about'>About</Link></li>
-          <li className={`underline-offset-3 ${isActive("/events") ? "underline" : "hover:underline"}`}><Link href='/events'>Events</Link></li>
-      </ul>
+      <section className="flex justify-end">
+        {/** For mobile screens */}
+        <div className="sm:hidden mr-7">
+          <img src='icons/menu.png' alt='menu icon' className='h-[40px]' id="menu-open"></img>
+          <img src='icons/close.png' alt='close icon' className='h-[40px]' id="menu-close"></img>
+        </div>
+
+        {/** For desktop screens */}
+        <div className='hidden sm:block'>
+          <ul className="flex items-center mr-7 text-navy-blue text-l font-medium font-mono">
+            <li className={`mr-5 underline-offset-3 ${isActive("/") ? "underline" : "hover:underline"}`}><Link href='/'>Home</Link></li>
+            <li className={`mr-5 underline-offset-3 ${isActive("/about") ? "underline" : "hover:underline"}`}><Link href='/about'>About</Link></li>
+            <li className={`underline-offset-3 ${isActive("/events") ? "underline" : "hover:underline"}`}><Link href='/events'>Events</Link></li>
+          </ul>
+        </div>
+      </section>
     </nav>
   );
 }
+
+const navOpen = false;
+
+// function openNav () {
+//   const menuOpen = document.getElementById("menu-open");
+//   const menuClose = document.getElementById("menu-close");
+
+//   if (navOpen == false) {
+//     menuOpen.addEventListener("click")
+//   }
+// }
--- isaca/src/app/events/page.jsx
+++ isaca/src/app/events/page.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 export default function Events() {
   return (
     <div className="flex flex-col items-center min-h-screen p-14">
-      <h1 className="text-4xl font-bold-DM mono text-[#0C2A64] mb-2">Events</h1>
-      <p className="text-gray-500 text-md mt-2 pb-10 font-DM Sans">
-        Check out upcoming events. We do about 3-4 events a quarter! ...
-      </p>
-      <hr className="w-5/6 border-t border-gray-700 my-4 " />
+      <section className="mb-4" style = {{padding: "10px 20px",textAlign: "center" }}>
+        <h1 className="mb-2">Events</h1>
+        <p style={{
+            maxWidth: "700px",
+            margin: "0 auto",
+            fontSize: "18px",
+            lineHeight: "1.6",
+        }}>
+          Check out our upcoming events.
+          We usually host 3-4 events per quarter, focused on career development,
+          professional networking, and industry panels.
+        </p>
+      </section>
+
       <div>
-      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showCalendars=0&showNav=0&showPrint=0&title&src=OWNhYTNiYzYwNzUxMzZhODIyMWJhMTJiYzI4MmZhYTY2ZWVmYmQ1MTRkNTYzNGMxNmNjZWU4YWY2MTI5NWMyZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E"
-       style={{borderWidth:0}}
-       className = "shadow-xl luminousity rounded-2xl"
-       color='#f4511e'
-        width="1100" 
-        height="600"  
-        scrolling="no"></iframe>
-        </div>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showCalendars=0&showNav=0&showPrint=0&title&src=OWNhYTNiYzYwNzUxMzZhODIyMWJhMTJiYzI4MmZhYTY2ZWVmYmQ1MTRkNTYzNGMxNmNjZWU4YWY2MTI5NWMyZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E"
+          style={{borderWidth:'0.6px', borderColor:'#0C2A64'}}
+          className = "shadow-md luminousity rounded-2xl"
+          color='#005288'
+          width="1000"
+          height="550"
+        >
+        </iframe>
+      </div>
     </div>
   );
 }
