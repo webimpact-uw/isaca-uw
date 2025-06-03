574f77923fd0528b2c3c1a2f5f4ea4d50a712b8d
bbgmqus <150094417+bbgmqus@users.noreply.github.com>
2025-06-02 18:40:08 -0700

    Updated calendar to the new isaca gmail

--- isaca/src/app/events/page.jsx
+++ isaca/src/app/events/page.jsx
@@ -31,7 +31,7 @@ export default function Events() {
       </section>
 
       <div className="hidden md:block" id="large-calendar">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showCalendars=0&showNav=0&showPrint=0&title&src=OWNhYTNiYzYwNzUxMzZhODIyMWJhMTJiYzI4MmZhYTY2ZWVmYmQ1MTRkNTYzNGMxNmNjZWU4YWY2MTI5NWMyZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E"
+       <iframe src="https://calendar.google.com/calendar/embed?src=5d91a9f1ad306eb88105801f540046847dad65e98f846c436c71369258bbaeba%40group.calendar.google.com&ctz=America%2FLos_Angeles"
           style={{borderWidth:'0.6px', borderColor:'#0C2A64'}}
           className = "shadow-md luminousity rounded-2xl"
           color='#005288'
@@ -41,7 +41,7 @@ export default function Events() {
       </div>
 
       <div className="sm:hidden" id="small-calendar">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&showTitle=0&showNav=0&mode=AGENDA&src=OWNhYTNiYzYwNzUxMzZhODIyMWJhMTJiYzI4MmZhYTY2ZWVmYmQ1MTRkNTYzNGMxNmNjZWU4YWY2MTI5NWMyZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E"
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&mode=AGENDA&src=NWQ5MWE5ZjFhZDMwNmViODgxMDU4MDFmNTQwMDQ2ODQ3ZGFkNjVlOThmODQ2YzQzNmM3MTM2OTI1OGJiYWViYUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23c0ca33"
           style={{borderWidth:'0.6px', borderColor:'#0C2A64'}}
           className = "shadow-md luminousity rounded-2xl"
           color='#005288'
